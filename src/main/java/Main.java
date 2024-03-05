b5e15584e2b1db8a38293b6f56456951d6954292
Adam <adam.ewing.nz@gmail.com>
2024-03-05 10:54:08 +0800

    First commit.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        // call the first add method and store the result into a variable called resultOne.
+
+        // print resultOne.
+
+        // call the greeting method and store the result into a variable called resultTwo.
+
+        // print resultTwo.
+
+        // call the second add method and store the result into a variable called resultThree.
+
+        // print resultThree.
+
+        // call the printCapitalised method.
+
+    }
+
+    /**
+     * add
+     * @param numOne - an integer
+     * @param numTwo - an integer
+     * @return the sum of numOne and numTwo (an integer)
+     */
+    public static int add(int numOne, int numTwo) {
+
+        return numOne + numTwo;
+
+    }
+
+    /**
+     * greeting
+     * @param name - the name of a person (a String)
+     * @return a greeting in the format of "Bonjour, [name]!" (a String)
+     */
+    public static String greeting(String name) {
+
+        return "Bonjour, " + name + "!";
+
+    }
+
+    /**
+     * add
+     * @param numOne - an integer
+     * @param numTwo - an integer
+     * @param numThree - an integer
+     * @param numFour - an integer
+     * @return the sum of numOne, numTwo, numThree, and numFour (an integer)
+     */
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {
+
+        return add(add(numOne, numTwo), add(numThree, numFour));
+
+    }
+
+    /**
+     * printCapitalised
+     * @param toBePrinted - a String that the user wishes to be printed in all caps.
+     */
+    public static void printCapitalised(String toBePrinted) {
+
+        System.out.println(toBePrinted.toUpperCase());
+
+    }
+
+
+
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,4 +1,29 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void add() {
+
+        assertEquals(4, Main.add(2,2));
+        assertEquals(22, Main.add(20,2));
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void greeting() {
+
+        String expectedOne = "Bonjour, Emily!";
+        String expectedTwo = "Bonjour, Amboise!";
+        assertEquals(expectedOne, Main.greeting("Emily"));
+        assertEquals(expectedTwo, Main.greeting("Amboise"));
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAdd() {
+
+        assertEquals(8, Main.add(2,2,2,2));
+        assertEquals(56, Main.add(18, 18, 12, 8));
+    }
 }
